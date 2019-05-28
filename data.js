fc3e3928493bffe3c35fc104b9af0e81f8424f37
Mauricio Soares <m.soares2603@gmail.com>
2019-05-28 20:35:31 +0200

    stuff

---
+++ data.js
@@ -0,0 +1,169 @@
+window.products = [{
+  images: 3,
+  description: 'In perfect conditions :)',
+  name: 'Wooden Trolley Table',
+  paidPrice: 300,
+  sellingPrice: 100,
+  storeLink: 'https://www.hoeffner.de/artikel/12343517',
+  availability: 'Today'
+}, {
+  images: 1,
+  description: `Unfortunately one of the stool is not keeping the height once you adjust it, and it has some cat scratches.`,
+  name: 'Bar stool (For both)',
+  paidPrice: 80,
+  sellingPrice: 20,
+  storeLink: 'https://www.hoeffner.de/artikel/13318178',
+  availability: 'July'
+}, {
+  images: 2,
+  description: 'In perfect conditions, it has a lot of space for drinks and sweets :)',
+  name: 'Mini Refrigerator',
+  paidPrice: 180,
+  sellingPrice: 60,
+  storeLink: 'https://shopping.prokira.de/elektrogeraete/kuehl-gefriergeraete/431395/wolkenstein-ks95rt-lb-kuehlschrank-mit-gefrierfach/retro-popart/freistehend/hoehe-88-cm/a/92-l?sPartner=google&gclid=Cj0KCQjwuLPnBRDjARIsACDzGL2sJP1wHDmkh2R6WK3X0T8jFC0y3hZfDon_WyEc_JQTMgwmx5qdyfIaAtVyEALw_wcB',
+  availability: 'Today'
+}, {
+  images: 1,
+  description: `If you're taking this guitar, please make sure that it will be used, and not just for decoration :)`,
+  name: 'Acoustic Guitar (FREE)',
+  paidPrice: 50,
+  sellingPrice: 0,
+  storeLink: 'https://www.thomann.de/de/startone_cg_851_4_4_set_1.htm',
+  availability: 'Today'
+}, {
+  images: 1,
+  description: 'This was used a lot by our cat, so probably has a lot fur, but he had lots of fun in there, I bet your cat will too',
+  name: 'Cat Tree House',
+  paidPrice: 60,
+  sellingPrice: 20,
+    storeLink: 'https://www.amazon.de/CO-Z-Kletterbaum-Kratzbaum-mit-ngematte/dp/B07CLVX1Z7/ref=sr_1_4_sspa?keywords=kratzbaum+house&qid=1559063769&s=gateway&sr=8-4-spons&psc=1',
+    availability: 'Today'
+}, {
+  images: 2,
+  description: 'The couch is in very good conditions, however in one of the arm it has a bit of "pink" ink. All the cloth can be removed and washed separately.',
+  name: 'Couch',
+  paidPrice: 470,
+  sellingPrice: 120,
+  storeLink: 'https://www.ikea.com/de/de/p/norsborg-3er-sofa-finnsta-weiss-weiss-birke-s39124943/',
+  availability: 'July'
+}, {
+  images: 1,
+  description: 'From Ikea, these are in excelent conditions, and they have one extenstion on top',
+  name: '4 Bookshelves (With 1 extension)',
+  paidPrice: 200,
+  sellingPrice: 70,
+  storeLink: 'https://www.ikea.com/de/de/p/billy-buecherregal-braun-eschenfurnier-10323349/',
+  availability: 'Today'
+}, {
+  images: 2,
+  description: `Living room coffee table in perfect conditions, and the material is very good quality (it's very heavy)`,
+  name: 'Coffee Table',
+  paidPrice: 215,
+  sellingPrice: 70,
+  storeLink: 'https://www.xxxlutz.de/p/carryhome-couchtisch-akazie-massiv-rechteckig-akaziefarben-bronzefarben-001944001106',
+  availability: 'Today'
+}, {
+  images: 2,
+  description: `Lowboard for your living room, it's the same style of the coffee table, and also the material is very good quality (very heavy)`,
+  name: 'Lowboard',
+  paidPrice: 240,
+  sellingPrice: 80,
+  storeLink: 'https://www.xxxlutz.de/p/carryhome-lowboard-akazie-naturfarben-001944001103',
+  availability: 'July'
+}, {
+  images: 2,
+  description: 'This TV I already bought used for 100 euros (which was cheap for the size and quality)',
+  name: 'TV (Bought it used)',
+  paidPrice: 100,
+  sellingPrice: 80,
+  storeLink: null,
+  availability: 'July'
+}, {
+  images: 2,
+  description: 'Simple HP all in one printer, you can scan, copy and print stuff :)',
+  name: 'All in one Printer',
+  paidPrice: 60,
+  sellingPrice: 20,
+  storeLink: 'https://www.lidl.de/de/hp-all-in-one-drucker-envy-5010/p292791?gclid=Cj0KCQjwuLPnBRDjARIsACDzGL1rqxrM9OkzItWyPuJbKnnjkhelW1_bz9taxs4ZDDBRnbjcfyrDEFMaArDTEALw_wcB',
+  availability: 'Today'
+}, {
+  images: 2,
+  description: `Very confortable office chair, the arms can be lifted up. It's making some noise when you move it, but some oil probably fixes it`,
+  name: 'Office Chair',
+  paidPrice: 100,
+  sellingPrice: 30,
+  storeLink: null,
+  availability: 'July'
+}, {
+  images: 2,
+  description: 'Very spacious computer desk, as you can see in the photo, you can have up to 2 computers on it. The wood quaity is excelent',
+  name: 'Computer Desk',
+  paidPrice: 160,
+  sellingPrice: 60,
+  storeLink: 'https://www.xxxlutz.de/p/linea-natura-schreibtisch-buche-massiv-buchefarben-001665000401',
+  availability: 'July'
+}, {
+  images: 2,
+  description: 'This keyboard is brand new, was bought a couple of months ago, and unfortunately was not very used... Comes with the stand and the chair',
+  name: 'Yamaha NP-12 B Set',
+  paidPrice: 200,
+  sellingPrice: 80,
+  storeLink: 'https://www.musicstore.de/en_DE/EUR/Yamaha-NP-12-B/art-KEY0004442-000V000112476',
+  availability: 'Today'
+}, {
+  images: 3,
+  description: `Bought this guitar ~2 years ago, it's in very good conditions`,
+  name: 'Guitar Schecter C-6',
+  paidPrice: 380,
+  sellingPrice: 120,
+  storeLink: 'https://www.musicstore.de/en_DE/EUR/Schecter-C-6-Deluxe-Satin-Black/art-GIT0036854-000',
+  availability: 'Today'
+}, {
+  images: 3,
+  description: 'This projector is used in my bedroom for watching movies. Unfortunately the fans make quite a lot of noise, and the image has a very small smudge on it, but the image quality is excelent (720p)',
+  name: 'CRENOVA Projector',
+  paidPrice: 200,
+  sellingPrice: 70,
+  storeLink: null,
+  availability: 'Today'
+}, {
+  images: 2,
+  description: 'Simple night stand, in good conditions',
+  name: 'Night Stand',
+  paidPrice: 40,
+  sellingPrice: 15,
+  storeLink: 'https://www.ikea.com/de/de/p/malm-kommode-mit-2-schubladen-weiss-80214549/',
+  availability: 'Today'
+}, {
+  images: 3,
+  description: 'In excellent conditions, comes with 1 table and 2 stools',
+  name: 'Biergarten Style table',
+  paidPrice: 60,
+  sellingPrice: 20,
+  storeLink: null,
+  availability: 'Today'
+}, {
+  images: 4,
+  description: `This wardrobe was made using the custom IKEA tool. We bought the pieces separately, the link below you'll find the receipt with all the items bought`,
+  name: 'Wardrobe (3 pieces)',
+  paidPrice: 1170,
+  sellingPrice: 400,
+  storeLink: 'https://drive.google.com/file/d/1QP9KgaS7_lnbLAu1lnWTYhYUdMQ0t7hE/view?usp=sharing',
+  availability: 'July'
+}, {
+  images: 2,
+  description: 'Just a bed, in good conditions :)',
+  name: 'Bed',
+  paidPrice: 200,
+  sellingPrice: 70,
+  storeLink: 'https://www.ikea.com/de/de/p/hemnes-bettgestell-weiss-gebeizt-s89931560/',
+  availability: 'July'
+}, {
+  images: 4,
+  description: 'This Wash/Dryier is excellent! Works perfectly, and can dry a good amount of clothes at once',
+  name: 'Wash/Dryer Machine',
+  paidPrice: 600,
+  sellingPrice: 250,
+  storeLink: null,
+  availability: 'July'
+}]
---
+++ script.js
@@ -0,0 +1,99 @@
+class Hello extends React.Component {
+  componentDidMount() {
+    document.querySelectorAll('.carousel').forEach(item => {
+      const nextArrow = document.querySelector('#arrow-next-' + item.id)
+      const prevArrow = document.querySelector('#arrow-prev-' + item.id)
+      if (item.querySelectorAll('img').length === 1) {
+        nextArrow.parentNode.removeChild(nextArrow)
+        prevArrow.parentNode.removeChild(prevArrow)
+        return
+      }
+      const siema = new Siema({
+        selector: '#' + item.id
+      });
+      nextArrow.addEventListener('click', () => {
+        siema.next()
+      })
+
+      prevArrow.addEventListener('click', () => {
+        siema.prev()
+      })
+    });
+
+    document.querySelectorAll('.description').forEach(item => {
+      item.addEventListener('click', () => {
+        const isOpen = item.classList.value.indexOf('open') !== -1
+        if (isOpen) {
+          item.classList.remove('open')
+        } else {
+          item.classList.add('open')
+        }
+      })
+    });
+  }
+  render() {
+    return (
+      <div className="products">
+        {window.products.map((p, i) => (
+          <div className="product" key={i}>
+            <div className="carousel-holder">
+              <div id={`arrow-next-p${i + 1}`} className="arrows arrow-right">
+                <img src="next.png" />
+              </div>
+              <div id={`arrow-prev-p${i + 1}`} className="arrows arrow-left">
+                <img src="next.png" />
+              </div>
+              <div id={`p${i + 1}`} className="carousel">
+                {Array(p.images).fill('').map((item, imageIndex) => (
+                  <img loading="lazy" key={imageIndex} src={`images/p${i + 1}${imageIndex + 1}.jpg`} />
+                ))}
+              </div>
+              <div className="description">
+                <div className="show">Show Description</div>
+                <div className="hide">Hide Description</div>
+                {p.description}
+              </div>
+            </div>
+            <div className="details">
+              <div className="name">
+                {p.name}
+              </div>
+              <div className="prices">
+                <div className="original-price">
+                  Paid Price: <span className="red">{p.paidPrice} EUR</span>
+                </div>
+                <div className="price">
+                  Selling Price: <span className="green">{p.sellingPrice} EUR</span>
+                </div>
+                <div className="link-to-product">
+                  {p.storeLink ?
+                    <a href={p.storeLink} target="_blank">
+                      Link to store where it was bought
+                    </a> :
+                    <span>No Link</span>
+                  }
+                </div>
+                <div className="availability">
+                  Availability: <b className={ p.availability === 'Today' ? 'green' : 'red'}>{p.availability}</b>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+         <Hello text="World" />,
+document.getElementById('root')
+);
+
+
+var modalTinyNoFooter = new tingle.modal();
+var btn = document.querySelector('.open-modal');
+btn.addEventListener('click', function () {
+  modalTinyNoFooter.open();
+});
+modalTinyNoFooter.setContent(document.querySelector('.modal').innerHTML);
